6f9829f0605ef6ff4d4fdc5dd31400391318e977
Yasmin Garza <y.garza93@outlook.com>
2023-01-02 17:26:20 -0600

    functionality
    Add Outlets, actions, textfield delegate protocol

--- Weather/ViewController.swift
+++ Weather/ViewController.swift
@@ -7,13 +7,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    @IBOutlet weak var conditionImage: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var searchTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        searchTextField.delegate = self
     }
 
-
+    @IBAction func seachPressed(_ sender: UIButton) {
+        searchTextField.endEditing(true)
+        print(searchTextField.text)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.endEditing(true)
+        guard let searchValue = textField.text else {
+            return false
+        }
+        print(searchValue)
+        return true
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField.text != "" {
+            return true
+        } else {
+            // should type something
+            return false
+        }
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        textField.text = nil
+    }
+    
 }
 
